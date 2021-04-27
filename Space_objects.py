3000ecccb3f57268bdab23e16e1f80ea1d767085
Arseniy <plakhotniuk.av@phystech.edu>
2021-04-27 16:46:21 +0300

    новая отрисовка, пока без движения камеры

--- Space_objects.py
+++ Space_objects.py
@@ -1,33 +1,66 @@
-from vpython import scene, vector, sphere, color, rate, mag
+try:
+    import OpenGL as ogl
+    try:
+        import OpenGL.GL
+    except ImportError:
+        print('Drat, patching for Big Sur')
+        from ctypes import util
+        orig_util_find_library = util.find_library
 
-
-class SolarSystem:
+        def new_util_find_library(name):
+            res = orig_util_find_library(name)
+            if res:
+                return res
+            return '/System/Library/Frameworks/'+name+'.framework/'+name
+        util.find_library = new_util_find_library
+except ImportError:
     pass
 
+import OpenGL.GLU
 
-class SpaceObject:
+
+WHITE = (1, 1, 1)
+RED = (1, 0, 0)
+BLUE = (0, 0, 1)
+YELLOW = (1, 1, 0)
+GREEN = (0, 1, 0)
+DARKGREEN = (0, 100, 0)
+MAGENTA = (1, 0, 1)
+CYAN = (0, 1, 1)
+BLACK = (0, 0, 0)
+COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
+
+
+class CelestialBody:
     """
-    Класс небесных тел,
-    Координаты, скорость и сила задаются векторами в R3
+    Класс небесного тела.
+    Содержит массу, координаты, скорость звезды,
+    а также визуальный радиус звезды в пикселах и её цвет.
     """
-    def __init__(self, mass=0, position=vector(0, 0, 0), velocity=vector(0, 0, 0),
-                 force=vector(0, 0, 0), radius=0, color='', name=''):
+    def __init__(self, m=0, x=0, y=0, vx=0, vy=0, fx=0, fy=0, r=0, color='', name=''):
 
-        """Изображение планеты"""
-
-        self.mass = mass
+        self.m = m
         """Масса планеты"""
 
-        self.position = position
-        """Координатный вектор небесного тела"""
+        self.x = x
+        """Координата по оси **x**"""
 
-        self.velocity = velocity
-        """Скорость"""
+        self.y = y
+        """Координата по оси **y**"""
 
-        self.force = force
-        """Сила"""
+        self.vx = vx
+        """Скорость по оси **x**"""
 
-        self.radius = radius
+        self.vy = vy
+        """Скорость по оси **y**"""
+
+        self.fx = fx
+        """Сила по оси **x**"""
+
+        self.fy = fy
+        """Сила по оси **y**"""
+
+        self.r = r
         """Радиус планеты"""
 
         self.color = color
@@ -36,13 +69,17 @@ class SpaceObject:
         self.name = name
         "Название планеты"
 
-    def draw(self):
-        obj = sphere(pos=self.position, radius=self.radius, velocity=self.velocity,
-                     color=self.color, make_trail=True, interval=10, retain=50)
+    def PlanetDraw(self):
+        """
+        Draws planets
+        """
+        sphere = OpenGL.GLU.gluNewQuadric()  # Create new sphere
+        OpenGL.GL.glPushMatrix()
+        OpenGL.GL.glTranslatef(self.x, 5.0E7, self.y)  # Move to the place
+        OpenGL.GL.glColor4f(self.color[0], self.color[1], self.color[2], 1)  # Put color
+        OpenGL.GLU.gluSphere(sphere, self.r * 10, 320, 160)  # Draw sphere (sphere, radius)
+        OpenGL.GL.glPopMatrix()
 
 
-
-
-
-
-
+if __name__ == "__main__":
+    print("This module is not for direct call!")
--- Starship.py
+++ Starship.py
@@ -1,44 +1,7 @@
-from vpython import scene, vector, sphere, color, rate, mag, cone, cylinder
+class Starship:
+    pass
 
 
-class Player:
-    def __init__(self, mass=0, position=vector(0, 0, 0), velocity=vector(0, 0, 0),
-                 force=vector(0, 0, 0), name=''):
-        self.mass = mass
-        """Масса космического корабля"""
-
-        self.position = position
-        """Координатный вектор космического корабля"""
-
-        self.velocity = velocity
-        """Скорость"""
-
-        self.force = force
-        """Сила, действующая на космический корабль"""
-        self.body = body = cylinder(pos=vector(0, 0, 0),
-                                    axis=vector(0, 0, 0) + vector(1, 1, 1),
-                        size=vector(20, 10, 10), color=color.white)
-
-    def draw(self):
-        """
-        Рисует космический корабль
-        :return:
-        """
-
-        body = cylinder(pos=vector(0, 0, 0), axis=vector(0, 0, 0) + vector(1, 1, 1),
-                        size=vector(20, 10, 10), color=color.white)
-        cone(pos=vector(0, 0, 0), axis=-vector(1, 1, 1),
-                         size=vector(10, 10, 10), color=color.white)
-        cone(pos=vector(0, 0, 0) + vector(12, 12, 12), axis=vector(0, 0, 0) - vector(1, 1, 1),
-             size=vector(22, 15, 7), color=color.white)
-
-        cone(pos=vector(0, 0, 0) + vector(12, 12, 12), axis=vector(0, 0, 0) - vector(1, 1, 1),
-             size=vector(22, 7, 15), color=color.white)
-
-    def camera_tracking(self):
-        """
-        Устанавливает слежение камеры за кораблем
-        :return:
-        """
-        scene.camera.follow(self.body)
+if __name__ == "__main__":
+    print("This module is not for direct call!")
 
